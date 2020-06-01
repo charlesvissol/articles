f650e392c8c9d64a8e0cf0b0c9a2ed3212633f53
Charles Vissol <64405521+charlesvissol@users.noreply.github.com>
2020-06-01 19:10:18 +0200

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,27 @@
+package org.angrybee.test.jackson;
+
+public class Student {
+
+	private String firstname;
+	
+	private String lastname;
+	
+	
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	
+}
